a115e0230389669652e1630690f6782d3c9579c3
Adam Winick <sinelanguage@gmail.com>
2022-12-07 21:45:41 -0500

    Modularizef

--- IPTVFLX/ContentView.swift
+++ IPTVFLX/ContentView.swift
@@ -8,63 +8,12 @@
 import SwiftUI
 import CoreData
 
+
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    let val: Int = 47
-
-    let tempTxt: String =
-        """
-        FINALLY GOT
-        A VIEW TO RENDER ON THIS
-        FUCKING TV
-        FOR FUCK SAKES
-        at age
-        """
-    
-    let arr: [String] = ["Hello", "World", "SwiftUI"]
-    
-    let aSet = Set(["Hello", "World"])
-    
-    let aDict = [
-        "first": "Adam Winick",
-        "wife": "Sylvia Oliveira"
-    ]
-    
-    // Initial empty collections (complex types)
-    let emptyArr = [String]()
-    let emptyDict = [String: Int]()
-    let emptySet = Set<String>()
-    
-    let failure = "failure"
-    let success = "success"
-    
-    // Enums
-    enum AEnum {
-        case failure
-        case success
-    }
-    
-    /*
-     Enums can also store associated values attached to
-     each case. This lets you attach additional
-     information to your enums so they can represent more
-     nuanced data.
-     */
-    
-    enum AssociatedEnumExample {
-        case bored
-        case running(typeOfRunning: String)
-        case gymrat(typeOfExercise: String)
-    }
-    
-    // Now you can be specific when accessing Enum properites
-    let WeightLifting = AssociatedEnumExample.gymrat(typeOfExercise: "Bench Press")
-    
-    
-    // Only give dictionaries default values, if they are being evaluated, not
-    // Just for the fuck of it in a single LOC.
     var body: some View {
-        Text(tempTxt + " \(val) \(arr[0]) \(arr[1]) \(aDict["wife", default: "YO"])")
+        TVText
     }
 }
 
---
+++ IPTVFLX/SwiftLearnModule.swift
@@ -0,0 +1,93 @@
+//
+//  swiftLearn.swift
+//  IPTVFLX
+//
+//  Created by Adam Winick on 2022-12-07.
+//
+
+import Foundation
+import SwiftUI
+
+let arr: [String] = ["Hello", "World", "SwiftUI"]
+let val: Int = 47
+
+let tempTxt: String =
+    """
+    FINALLY GOT
+    A VIEW TO RENDER ON THIS
+    FUCKING TV
+    FOR FUCK SAKES
+    at age
+    """
+
+
+let aSet = Set(["Hello", "World"])
+
+let aDict = [
+    "first": "Adam Winick",
+    "wife": "Sylvia Oliveira"
+]
+
+// Initial empty collections (complex types)
+let emptyArr = [String]()
+let emptyDict = [String: Int]()
+let emptySet = Set<String>()
+
+// Enums
+enum AEnum {
+    // WTF Enums default to strings?
+    // Nope, according to docs, values are hashable by default
+    case failure
+    case success
+}
+
+/*
+ Enums can also store associated values attached to
+ each case. This lets you attach additional
+ information to your enums so they can represent more
+ nuanced data.
+ */
+
+enum AssociatedEnumExample {
+    case bored
+    case running(typeOfRunning: String)
+    case gymrat(typeOfExercise: String)
+}
+
+
+// Enum raw values initiated after
+// Now you can be specific when accessing Enum properites
+let WeightLifting = AssociatedEnumExample.gymrat(typeOfExercise: "Bench Press")
+
+
+// Functions
+func printString(str: String) -> String {
+    return str
+}
+
+// Return type could be a Tuple
+func returnTuple() -> (first: String, second: String) {
+    return (first: "hello", second: "world")
+}
+
+func returnTupleB(_ noVarName: String, withVarName: String) -> String {
+    return (noVarName + " " + withVarName)
+}
+
+// Desctructure the return value of the above function
+// You can skip values too
+
+let (first, _) = returnTuple()
+let (_, second) = returnTuple()
+let noNamedParam = returnTupleB("No String", withVarName: "Param")
+
+// Only give dictionaries default values, if they are being evaluated, not
+// Just for the fuck of it in a single LOC.
+let TVText = Text(
+    tempTxt +
+        """
+        \(val) \(arr[0]) \(arr[1]) \(aDict["wife", default: "YO"])
+        \(AEnum.success) \(printString(str: "SHMEE"))
+        \(first) \(second) \(noNamedParam) \(ExportedString)
+        """
+    )
